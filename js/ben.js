df532078292c815f168a363e9986ef417c0571ef
caowei666 <1029142064@qq.com>
2018-10-10 22:32:30 +0800

    1010提交的项目

--- js/ben.js
+++ js/ben.js
@@ -5,4 +5,31 @@ $(function(){
             display:"none"
         })
     })
+// 滑到本来生活弹出二维码
+    $(".ulli3").hover(
+        function(){
+            $(".ulli3>div").css({
+                display:"block"
+            })
+        },
+        function(){
+            $(".ulli3>div").css({
+                display:"none"
+            })
+        }
+    )
+    $(".top-con .right li:nth-of-type(4)").hover(
+        function(){
+            $(".top-con .right li:nth-of-type(4) div").css({
+                display:"block"
+            })
+        },
+        function(){
+            $(".top-con .right li:nth-of-type(4) div").css({
+                display:"none"
+            })
+        }
+    )
+
 })
+
